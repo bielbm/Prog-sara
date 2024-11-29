c4794c2205b1c4b35f9030f5933d73b4630213fc
bielbm <163012253+bielbm@users.noreply.github.com>
2024-11-29 16:39:12 -0300

    Add files via upload

---
+++ prog sara2.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(){
+int a;
+int b;
+int c;
+int d;
+int e;
+int i;
+printf("Digite um numero para que possa saber quantos deles sao negativo:\n");
+scanf("%d", &a);
+printf("Digite um numero para que possa saber quantos deles sao negativo:\n");
+scanf("%d", &b);
+printf("Digite um numero para que possa saber quantos deles sao negativo:\n");
+scanf("%d", &c);
+printf("Digite um numero para que possa saber quantos deles sao negativo:\n");
+scanf("%d", &d);
+printf("Digite um numero para que possa saber quantos deles sao negativo:\n");
+scanf("%d", &e);
+ 
+    if(a<0){
+     printf("Seu numero é negativo %d\n", a);
+    } else{
+     printf("Seu numro é positivo %d\n", a);
+    }
+    if(b<0){
+     printf("Seu numero é negativo %d\n", b);
+    } else{
+     printf("Seu numro é positivo %d\n", b);
+    }
+    if(c<0){
+     printf("Seu numero é negativo %d\n", c);
+    } else{
+     printf("Seu numro é positivo %d\n", c);
+    }
+    if(d<0){
+     printf("Seu numero é negativo %d\n", d);
+    } else{
+     printf("Seu numro é positivo %d\n", d);
+    }
+    if(e<0){
+     printf("Seu numero é negativo %d\n", e);
+    } else{
+     printf("Seu numro é positivo %d\n", e);
+    }
+    return 0;
+}
